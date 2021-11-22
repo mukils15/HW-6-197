834cd1fee5fd709756f1f96c948dc3cf7bc6e9ba
Mukil Shanmugam <45906343+mukils15@users.noreply.github.com>
2021-11-21 20:24:25 -0500

    Delete backend directory

--- backend/middlewares/isAuthenticated.js
+++
@@ -1,9 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-        if (req.session === undefined || req.session.username === undefined || req.session.username === ""){
-            next(new Error("Something didn't work!"))
-        } else {
-            next()
-        }
-}
-
-module.exports = isAuthenticated
